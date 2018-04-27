51f3fccfdb70f3d73d3dec348a1f1057b63ea26c
kentkent <kent@solody.com>
2018-04-27 16:39:03 +0800

    迁移功能

---
+++ src/Controller/TaskRedirectController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\supplier\Controller;
+
+use Drupal\commerce_store\Entity\Store;
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class TaskRedirectController.
+ */
+class TaskRedirectController extends ControllerBase
+{
+
+    /**
+     * Owner.
+     *
+     * @param Store $commerce_store
+     * @return string
+     *   Return Hello string.
+     */
+    public function owner(Store $commerce_store)
+    {
+        $user = $commerce_store->getOwner();
+        return $this->redirect('entity.user.edit_form', ['user' => $user->id()]);
+    }
+
+
+    public function financeAccount(Store $commerce_store)
+    {
+        $query = \Drupal::entityQuery('finance_account');
+        $query->condition('user_id', $commerce_store->getOwnerId());
+        $query->condition('type', SUPPLIER_FINANCE_ACCOUNT_TYPE);
+        $entity_ids = $query->execute();
+
+        return $this->redirect('entity.finance_account.canonical', ['finance_account' => array_pop($entity_ids)]);
+    }
+}
